62b50025283aad13c9803b11ae843bcf3d921ab0
Etti Kehinde <ettiolayinka4@gmail.com>
2024-11-11 19:04:25 +0100

    Login functionality
    still in progress

--- Core/src/Frontend/Pages/LoginSignup.jsx
+++ Core/src/Frontend/Pages/LoginSignup.jsx
@@ -1,10 +1,20 @@
-
+import { useState } from 'react'
 
 
 export default function LoginSignUp(){
+    const [ email,setEmail ] = useState('')
+    const [ password,setPassword ] = useState('')
+
+    const logun
     return (
         <div>
-            <h1 style={{color: 'black'}}>It is working</h1>
+            <h1 style={{color: 'black'}}>Login</h1>
+
+            <form>
+                <input  type="email" name="email" id="" />
+                <input type="password" name="password" id="" />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     )
 }

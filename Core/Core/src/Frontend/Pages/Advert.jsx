3721a7be1761939abd04cc864c43e6698329165b
Etti Kehinde <ettiolayinka4@gmail.com>
2024-11-13 12:24:32 +0100

    fixed the core mistake

--- Core/Core/src/Frontend/Pages/Advert.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Advert(){
-    return (
-        <div>
-            <h1 style={{color: 'black'}}>Advert</h1>
-        </div>
-    )
-}
--- Core/Core/src/Frontend/Pages/EventsCategory.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function EventsCategory(){
-    return (
-        <div>
-            <h1 style={{color: 'black'}}>Discover</h1>
-        </div>
-    )
-}
